aacff73964a191228dba3c5b2473ebc4ca516120
Unknown <anamargaridarl@gmail.com>
2018-10-24 17:07:11 +0100

    added methods of Class person, user and teacher

--- src/Person.cpp
+++ src/Person.cpp
@@ -6,8 +6,79 @@
  */
 
 #include "Person.h"
+#include <iostream>
 
 using namespace std;
+
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+Person::Person(string name,int age,string gender)
+{
+	this->name=name;
+	this->age=age;
+	this->gender=gender;
+}
+
+string Person::getName()
+{
+	return name;
+}
+
+int Person::getAge()
+{
+	return age;
+}
+
+string Person::getGender()
+{
+	return gender;
+}
+
+void Person::setName(string name)
+{
+	this->name=name;
+}
+
+void Person::setAge(int age)
+{
+	this->age=age;
+}
+
+void  Person::setGender(string gender)
+{
+	this->gender=gender;
+}
+
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+Teacher::Teacher(string name, int age, string gender):Person(name,age,gender)
+{}
+
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+User::User(string name,int age,string gender, bool isGold):Person(name,age,gender)
+{
+	this->isGold = isGold;
+}
+
+void User::makeGold()
+{
+	isGold = true;
+}
+
+void User::stopGold()
+{
+	isGold = false;
+}
+
+
+
+
+
+
+
 
 
 
--- src/Person.h
+++ src/Person.h
@@ -11,7 +11,8 @@
 #include <string>
 #include <vector>
 
-class Person{
+class Person
+{
 public:
 	Person(std::string name,int age, std::string gender);
 	std::string getName();
@@ -20,13 +21,10 @@ public:
 	void setName(std::string name);
 	void setAge(int age);
 	void setGender(std::string gender);
-
-
 private:
 	std::string name;
 	int age;
 	std::string gender;
-
 };
 
 class User :public Person {
@@ -34,20 +32,21 @@ public:
 	User(std::string name,int age, std::string gender, bool isGold);
 	void makeGold();
 	void stopGold();
-	Report getReport(int month);
-	void setReport(Report report, int month);
+	//Report getReport(int month);
+	//void setReport(Report report, int month);
 private:
-	bool isGold();
-	std::vector<Report*> reports;
-	std::vector<Reservation*> reservations;
-	std::vector<Invoice*> invoices;
-
+	bool isGold;
+	//std::vector<Report*> reports;
+	//std::vector<Reservation*> reservations;
+	//std::vector<Invoice*> invoices;
+};
 
 class Teacher: public Person {
 public:
 	Teacher(std::string name, int age, std::string gender);
 	void makeReport(User user);
 private:
-	std::vector<Lesson*> lessons;
+	//std::vector<Lesson*> lessons;
+};
 
 #endif /* SRC_PERSON_H_ */
