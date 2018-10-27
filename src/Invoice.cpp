ab02787a2c431a78761b4969533874d2be41e977
Andre Rocha <andrefmrocha@live.com.pt>
2018-10-27 16:42:43 +0100

    Added Invoice class

--- src/Invoice.cpp
+++ src/Invoice.cpp
@@ -7,10 +7,32 @@
 
 #include "Invoice.h"
 
+using namespace std;
 
-Invoice::Invoice() {
-	// TODO Auto-generated constructor stub
-
+Invoice::Invoice(string name, string teacher, vector<Reservation *> reservs)
+{
+	this->name = name;
+	this->assignedTeacher = teacher;
+	this->reservs  = reservs;
+	this->totalPrice = 0;
+	for(auto i : reservs)
+	{
+		this->totalPrice+= i->getPrice();
+	}
 }
 
- /* namespace std */
+
+double Invoice::getPrice() const
+{
+	return this->totalPrice;
+}
+
+ostream & operator<<(ostream out, Invoice inv)
+{
+	out << "Descrimination of every use of the court by the user:" << endl;
+	for(unsigned int i = 0; i < inv.reservs.size(); i++)
+	{
+		out << i << " - " << inv.reservs[i]->day << "/" << inv.reservs[i]->month << " : " << inv.reservs[i]->getPrice() << endl;
+	}
+	out << "Final price:" << inv.getPrice();
+}
--- src/Invoice.h
+++ src/Invoice.h
@@ -7,11 +7,20 @@
 
 #ifndef SRC_INVOICE_H_
 #define SRC_INVOICE_H_
-
+#include <vector>
+#include <string>
+#include <iostream>
+#include "Reservation.h"
 
 class Invoice {
+	std::vector<Reservation *> reservs;
+	double totalPrice;
+	std::string name;
+	std::string assignedTeacher;
 public:
-	Invoice();
+	Invoice(std::string name, std::string teacherName, std::vector<Reservation *> reservs);
+	double getPrice() const;
+	friend std::ostream& operator<<(std::ostream out, Invoice inv);
 };
 
 
