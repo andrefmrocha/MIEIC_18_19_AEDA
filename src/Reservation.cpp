16131344e0fd95fb606f98028e8595d1fb6a7947
João Miguel Martins <up201707311@fe.up.pt>
2018-10-27 01:37:47 +0100

    Removed presence of Person class to avoid conflicts and allow compilation

--- src/Reservation.cpp
+++ src/Reservation.cpp
@@ -7,9 +7,8 @@
 
 #include "Reservation.h"
 
-Reservation::Reservation(User user,int month,int day,int startingHour,double price,unsigned int duration) : price(price)
+Reservation::Reservation(int month,int day,int startingHour,double price,unsigned int duration) : price(price)
 {
-	this->user = user;
 	this->month = month;
 	this->day = day;
 	this->startingHour = startingHour;
@@ -21,37 +20,16 @@ double Reservation::getPrice()
 	return price;
 }
 
-User Reservation::getUser()
-{
-	return user;
-}
-
-void Reservation::setUser(User user)
-{
-	this->user = user;
-}
-
-Lesson::Lesson(User u,int m,int d,int strHr,double p,unsigned int dr,Teacher t) : Reservation(u,m,d,strHr,p,dr)
-{
-	teacher = t;
-}
+Lesson::Lesson(int m,int d,int strHr,double p,unsigned int dr) : Reservation(m,d,strHr,p,dr)
+{}
 
 double Lesson::getPrice()
 {
 	return price;
 }
 
-Teacher Lesson::getTeacher()
-{
-	return teacher;
-}
 
-void Lesson::setTeacher(Teacher teacher)
-{
-	this->teacher = teacher;
-}
-
-Free::Free(User u,int m,int d,int strHr,double p,unsigned int dur) : Reservation(u,m,d,strHr,p,dur)
+Free::Free(int m,int d,int strHr,double p,unsigned int dur) : Reservation(m,d,strHr,p,dur)
 {
 
 }
--- src/Reservation.h
+++ src/Reservation.h
@@ -9,38 +9,31 @@
 #define SRC_RESERVATION_H_
 
 
-#include "Person.h"
+
 
 class Reservation {
 protected:
 	const double price;
 	unsigned int duration;
 private:
-	User user;
 	int month;
 	int day;
 	int startingHour;
 public:
-	Reservation(User user,int month,int day,int startingHour,double price,unsigned int duration);
+	Reservation(int month,int day,int startingHour,double price,unsigned int duration);
 	virtual double getPrice();
-	User getUser();
-	void setUser(User user);
 
 };
 
 class Lesson : public Reservation {
-private:
-	Teacher teacher;
 public:
-	Lesson(User u,int m,int d,int strHr,double price,unsigned int dr,Teacher t);
+	Lesson(int m,int d,int strHr,double price,unsigned int dr);
 	double getPrice();
-	Teacher getTeacher();
-	void setTeacher(Teacher teacher);
 };
 
 class Free : public Reservation {
 public:
-	Free(User u, int m,int d,int strHr,double p,unsigned int dur);
+	Free(int m,int d,int strHr,double p,unsigned int dur);
 	double getPrice();
 };
 
