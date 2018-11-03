251ec0515a99184d95e70b9b64b0cb133a1cbcbb
Unknown <anamargaridarl@gmail.com>
2018-11-03 19:40:22 +0000

    bug fix

--- src/Main.cpp
+++ src/Main.cpp
@@ -32,7 +32,7 @@ int MainIntro()
 	int flag;
 	cin>> flag;
 
-	while(flag!=1||flag!=2 ||flag!=3)
+	while(flag != 1 && flag != 2 && flag != 3)
 	{
 		cin.clear();
 		cin.ignore(1000, '\n');
@@ -53,13 +53,13 @@ int Menu(const int cardValue)
 	cout << "1.Add Person ----------------------------"<< endl;
 	cout << "2.Add Reservation -----------------------"<< endl;
 	cout << "3.Increment day--------------------------"<<endl;
-    //cout << "4.Go back--------------------------------"<<endl;
+	//cout << "4.Go back--------------------------------"<<endl;
 
 
 	int flag;
 	cin>> flag;
 
-	while(flag!=1||flag!=2||flag!=3)
+	while(flag!=1 && flag!=2 && flag!=3 && flag !=4)
 	{
 		cin.clear();
 		cin.ignore(1000, '\n');
@@ -92,146 +92,156 @@ int main()
 
 
 	////////////////////////////////////
-        int flag = MainIntro();
+	int flag = MainIntro();
 
-        switch (flag) {
-            case 1: {
-                //new company
-                int flagMenu = 0;
+	switch (flag)
+	{
+	case 1:
+	{
+		//new company
+		int flagMenu = 0;
 
+		Company C(cardValue, 2018);
 
-                Company C(cardValue, 2018);
+		while (flagMenu != 5)
+		{
+			/* if (flagMenu == 4) {
+       	   flag = 4;
+       	   break;
+      	  	  }
+			 */
 
-                while (flagMenu != 5) {
-                   /* if (flagMenu == 4) {
-                        flag = 4;
-                        break;
-                    }
-                    */
+			flagMenu = Menu(cardValue);
 
-                    flagMenu = Menu(cardValue);
+			/*
+			 * 1.Person
+			 * 2.Reservation
+			 * 3.Increment day
+			 */
+			switch (flagMenu)
+			{
+			case 1:
+			{
+				int flagP;
 
-                    /*
-                     * 1.Person
-                     * 2.Reservation
-                     * 3.Increment day
-                     */
-                    switch (flagMenu) {
-                        case 1: {
-                            int flagP;
+				cout << "1.Add User" << endl;
+				cout << "2.Add Teacher" << endl;
+				cout << "3.Go back" << endl;
 
-                            cout << "1.Add User" << endl;
-                            cout << "2.Add Teacher" << endl;
-                            cout << "3.Go back" << endl;
+				cin >> flagP;
 
-                            cin >> flagP;
+				while (flagP != 1 && flagP != 2 && flagP != 3)
+				{
+					cin.clear();
+					cin.ignore(1000, '\n');
+					cout << "Error...Try again: " << endl;
+					cin >> flagP;
+				}
 
-                            while (flagP != 1 || flagP != 2 || flag != 3) {
-                                cin.clear();
-                                cin.ignore(1000, '\n');
-                                cout << "Error...Try again: " << endl;
-                                cin >> flagP;
-                            }
+				if (flagP != 3)
+				{
 
-                            if (flag == 3)
-                                break;
+				cout << "Name: " << endl;
+				cin >> name;
+				cout << "Age: " << endl;
+				cin >> age;
+				cout << "Gender" << endl;
+				cin >> gender;
 
-                            cout << "Name: " << endl;
-                            cin >> name;
-                            cout << "Age: " << endl;
-                            cin >> age;
-                            cout << "Gender" << endl;
-                            cin >> gender;
+				if (flagP == 1) {
+					cout << "Gold Card?" << endl;
+					cout << "Option: 0.yes 1.no " << endl; //need to add go back
 
-                            if (flagP == 1) {
-                                cout << "Gold Card?" << endl;
-                                cout << "Option: 0.yes 1.no " << endl; //need to add go back
+					cin >> isGold;
 
-                                cin >> isGold;
+					if (!(C.registerUser(name, age, isGold, gender)))
+						cout << "Error adding User. Try again" << endl;
 
-                                if (!(C.registerUser(name, age, isGold, gender)))
-                                    cout << "Error adding User. Try again" << endl;
+				} else if (flagP == 2) {
+					if (!(C.registerTeacher(name, age, gender)))
+						cout << "Error adding User. Try again" << endl;
+				}
+				}
 
-                            } else if (flagP == 2) {
-                                if (!(C.registerTeacher(name, age, gender)))
-                                    cout << "Error adding User. Try again" << endl;
-                            }
+				break;
+			}
+			case 2:
+			{
+				int flagR;
 
-                            break;
-                        }
-                        case 2: {
-                            int flagR;
+				cout << "1.Add Free class" << endl;
+				cout << "2.Add Lesson" << endl;
+				cout << "3.Go back" << endl;
 
-                            cout << "1.Add Free class" << endl;
-                            cout << "2.Add Lesson" << endl;
-                            cout << "3.Go back" << endl;
+				cin >> flagR;
 
-                            cin >> flagR;
+				while (flagR != 1 && flagR != 2 && flagR != 3) {
+					cin.clear();
+					cin.ignore(1000, '\n');
+					cout << "Error...Try again: " << endl;
+					cin >> flagR;
+				}
 
-                            while (flagR != 1 || flagR != 2 || flagR != 3) {
-                                cin.clear();
-                                cin.ignore(1000, '\n');
-                                cout << "Error...Try again: " << endl;
-                                cin >> flagR;
-                            }
+				if (flagR == 3)
+					break;
 
-                            if (flagR == 3)
-                                break;
+				cout << "Month: " << endl;
+				cin >> m;
+				cout << "Day: " << endl;
+				cin >> d;
+				cout << "Starting Hour: " << endl;
+				cin >> strH;
+				cout << "Name of User: " << endl;
+				cin >> name;
 
-                            cout << "Month: " << endl;
-                            cin >> m;
-                            cout << "Day: " << endl;
-                            cin >> d;
-                            cout << "Starting Hour: " << endl;
-                            cin >> strH;
-                            cout << "Name of User: " << endl;
-                            cin >> name;
+				if (flagR == 1) {
+					//need to choose hot to handle price
+					cout << "Duration: " << endl;
+					cin >> duration;
+					//necessary to handle value entered in duration
+					if (!C.makeFree(m, d, strH, duration, name))
+						cout << "Error adding Free class. Try again" << endl;
 
-                            if (flagR == 1) {
-                                //need to choose hot to handle price
-                                cout << "Duration: " << endl;
-                                cin >> duration;
-                                //necessary to handle value entered in duration
-                                if (!C.makeFree(m, d, strH, duration, name))
-                                    cout << "Error adding Free class. Try again" << endl;
+				} else if (flagR == 2) {
+					//int i = C.getUser(name);
 
-                            } else if (flagR == 2) {
-                                //int i = C.getUser(name);
+					//need to choose hot to handle price
 
-                                //need to choose hot to handle price
-
-                                /*if(! C.makeLesson(month,day,strHr,name,C.getUsers().at(i)->getTeacher()))
+					/*if(! C.makeLesson(month,day,strHr,name,C.getUsers().at(i)->getTeacher()))
                                     cout << "Error adding Lesson. Try again"<< endl;
 
-                                    */
+					 */
 
 
-                            }
+				}
 
-                            break;
+				break;
 
-                        }
-                        case 3: {
-                            //Date++;
-                            break;
-                        }
-                    }
+			}
+			case 3:
+			{
+				//Date++;
+				break;
+			}
+			}
 
-                }
+		}
 
 
-                break;
+		break;
 
-            }
-            case 2: {
-                //load
-                break;
-            }
-            case 3: {
-                return 0;
-                break;
-            }
-        }
+	}
+	case 2:
+	{
+		//load
+		break;
+	}
+	case 3:
+	{
+		return 0;
+		break;
+	}
+	}
 
 
 
