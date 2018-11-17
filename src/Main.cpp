7baa34e7947621004ea175bc7388aff77838f692
Unknown <anamargaridarl@gmail.com>
2018-11-17 13:39:01 +0000

    bug fixing (error if function of company makeUserReport and makeUserInvoice are not comment (still nedd update) otherwise working

--- src/Main.cpp
+++ src/Main.cpp
@@ -18,70 +18,76 @@
 
 using namespace std;
 
-int MainIntro()
-{
-	cout << "-------------Tenis Company---------------"<< endl;
-	cout << "                                         "<< endl;
-	cout << "Choose option:                           "<< endl;
-	cout << "                                         "<< endl;
-	cout << "1.New Company                           "<< endl;
-	cout << "2.Load Company                            "<< endl;
-	cout << "3.Exit                                   "<< endl;
-	cout<< "------------------------------------------"<< endl;
+int MainIntro() {
+	cout << "-------------Tenis Company---------------" << endl;
+	cout << "                                         " << endl;
+	cout << "Choose option:                           " << endl;
+	cout << "                                         " << endl;
+	cout << "1.New Company                           " << endl;
+	cout << "2.Load Company                            " << endl;
+	cout << "3.Exit                                   " << endl;
+	cout << "------------------------------------------" << endl;
 
 	int flag;
-	cin>> flag;
+	cin >> flag;
 
-	while(flag != 1 && flag != 2 && flag != 3)
-	{
+	while (flag != 1 && flag != 2 && flag != 3) {
 		cin.clear();
 		cin.ignore(1000, '\n');
-		cout << "Error...Try again: "<< endl;
-		cin>>flag;
+		cout << "Error...Try again: " << endl;
+		cin >> flag;
 	}
 
 	return flag;
 
 }
 
-int Menu(const int cardValue)
-{
+int Menu(const int cardValue) {
 	cout << "-------------Company" << cardValue << "---------------" << endl;
-	cout << "                                         "<< endl;
-	cout << "Choose option:                           "<< endl;
-	cout << "                                         "<< endl;
-	cout << "1.Add Person                             "<< endl;
-	cout << "2.Add Reservation                        "<< endl;
-	cout << "3.Increment day                          "<<endl;
-    cout << "4.Exit                                   "<<endl;
-    cout << "-----------------------------------------"<< endl;
+	cout << "                                         " << endl;
+	cout << "Choose option:                           " << endl;
+	cout << "                                         " << endl;
+	cout << "1.Add Person                             " << endl;
+	cout << "2.Add Reservation                        " << endl;
+	cout << "3.Increment day                          " << endl;
+	cout << "4.Exit                                   " << endl;
+	cout << "-----------------------------------------" << endl;
 
 	//cout << "5.Go back--------------------------------"<<endl;
 
 
 	int flag;
-	cin>> flag;
+	cin >> flag;
 
-	while(flag!=1 && flag!=2 && flag!=3 && flag !=4)
-	{
+	while (flag != 1 && flag != 2 && flag != 3 && flag != 4) {
 		cin.clear();
 		cin.ignore(1000, '\n');
-		cout << "Error...Try again: "<< endl;
-		cin>>flag;
+		cout << "Error...Try again: " << endl;
+		cin >> flag;
 	}
 
 	return flag;
 
 }
 
-void DevelopCompany(Company &C)
-{
+int DevelopCompany(Company &C,Date &D, int cardValue) {
 
-	while (flagMenu != 5) {
-		/*if (flagMenu == 4) {
-		 flag = 4;
-		 break;
-		 }*/
+	string name, gender;
+	int age;
+	bool isGold;
+	int m, d, strH;
+	//double price;
+	//duration -- unsigned int --problem?
+	int duration;
+	int save;
+
+
+
+
+
+	int flagMenu = 0;
+
+	while (true) {
 
 		flagMenu = Menu(cardValue);
 
@@ -89,119 +95,127 @@ void DevelopCompany(Company &C)
 		//* 2.Reservation
 		//* 3.Increment day
 		//
+
+
 		switch (flagMenu) {
-		case 1: {
-			int flagP;
+			case 1: {
+				int flagP;
 
-			cout << "1.Add User" << endl;
-			cout << "2.Add Teacher" << endl;
-			cout << "3.Go back" << endl;
+				//small menu
 
-			cin >> flagP;
+				cout << "1.Add User" << endl;
+				cout << "2.Add Teacher" << endl;
+				cout << "3.Go back" << endl;
 
-			while (flagP != 1 && flagP != 2 && flagP != 3) {
-				cin.clear();
-				cin.ignore(1000, '\n');
-				cout << "Error...Try again: " << endl;
 				cin >> flagP;
-			}
 
-			if (flagP != 3) {
-
-				cout << "Name: " << endl;
-				cin >> name;
-				cout << "Age: " << endl;
-				cin >> age;
-				cout << "Gender" << endl;
-				cin >> gender;
-
-				if (flagP == 1) {
-					cout << "Gold Card?" << endl;
-					cout << "Option: 0.yes 1.no " << endl; //need to add go back
-
-					cin >> isGold;
-
-					if (C.getTeachers().empty()) {
-						cout
-								<< "Can't added Users.Company needs teachers first."
-								<< endl;
-						cout << "                                         "
-								<< endl;
-					} else if (!(C.registerUser(name, age, isGold, gender)))
-						cout << "Error adding User. Try again" << endl;
-
-				} else if (flagP == 2) {
-					if (!(C.registerTeacher(name, age, gender)))
-						cout << "Error adding User. Try again" << endl;
+				while (flagP != 1 && flagP != 2 && flagP != 3) {
+					cin.clear();
+					cin.ignore(1000, '\n');
+					cout << "Error...Try again: " << endl;
+					cin >> flagP;
 				}
+				//
+
+				if (flagP != 3) {
+
+					cout << "Name: " << endl;
+					cin >> name;
+					cout << "Age: " << endl;
+					cin >> age;
+					cout << "Gender" << endl;
+					cin >> gender;
+
+					if (flagP == 1) {
+						cout << "Gold Card?" << endl;
+						cout << "Option: 1.yes 0.no " << endl;
+
+						cin >> isGold;
+
+						if (C.getTeachers().empty()) {
+							cout << "Can't added Users. Company needs teachers first."
+								 << endl;
+							cout << "                                         "
+								 << endl;
+						} else if (!(C.registerUser(name, age, isGold, gender)))
+							cout << "Error adding User. Try again" << endl;
+
+					} else if (flagP == 2) {
+						if (!(C.registerTeacher(name, age, gender)))
+							cout << "Error adding User. Try again" << endl;
+					}
+				}
+
+				break;
 			}
+			case 2: {
 
-			break;
-		}
-		case 2: {
-			int flagR;
+				//small menu
+				int flagR;
 
-			cout << "1.Add Free class" << endl;
-			cout << "2.Add Lesson" << endl;
-			cout << "3.Go back" << endl;
+				cout << "1.Add Free class" << endl;
+				cout << "2.Add Lesson" << endl;
+				cout << "3.Go back" << endl;
 
-			cin >> flagR;
-
-			while (flagR != 1 && flagR != 2 && flagR != 3) {
-				cin.clear();
-				cin.ignore(1000, '\n');
-				cout << "Error...Try again: " << endl;
 				cin >> flagR;
-			}
 
-			if (flagR == 3)
+				while (flagR != 1 && flagR != 2 && flagR != 3) {
+					cin.clear();
+					cin.ignore(1000, '\n');
+					cout << "Error...Try again: " << endl;
+					cin >> flagR;
+				}
+				//
+
+				if (flagR != 3) {
+
+					cout << "Month: " << endl;
+					cin >> m;
+					cout << "Day: " << endl;
+					cin >> d;
+					cout << "Starting Hour: " << endl;
+					cin >> strH;
+					cout << "Name of User: " << endl;
+					cin >> name;
+
+					if (flagR == 1) {
+						//need to choose how to handle price
+						cout << "Duration: " << endl;
+						cin >> duration;
+						//necessary to handle value entered in duration
+						if (!C.makeFree(m, d, strH, duration, name))
+							cout << "Error adding Free class. Try again" << endl;
+
+					} else if (flagR == 2) {
+
+						int i = C.getUser(name);
+						//need to choose how to handle price
+
+						if (!C.makeLesson(m, d, strH, name, C.getUsers().at(i).getTeacher()))
+							cout << "Error adding Lesson. Try again" << endl;
+
+					}
+				}
+
 				break;
 
-			cout << "Month: " << endl;
-			cin >> m;
-			cout << "Day: " << endl;
-			cin >> d;
-			cout << "Starting Hour: " << endl;
-			cin >> strH;
-			cout << "Name of User: " << endl;
-			cin >> name;
+			}
+			case 3: {
+				++D;
+				break;
+			}
+			case 4: {
+				cout << "Save and exit? " << endl;
+				cout << "1.Yes 0.No" << endl;
+				cin >> save;
 
-			if (flagR == 1) {
-				//need to choose hot to handle price
-				cout << "Duration: " << endl;
-				cin >> duration;
-				//necessary to handle value entered in duration
-				if (!C.makeFree(m, d, strH, duration, name))
-					cout << "Error adding Free class. Try again" << endl;
-
-				/*} else if (flagR == 2) {
-				 int i = C.getUser(name);
-
-				 //need to choose hot to handle price
-
-				 if(! C.makeLesson(month,day,strHr,name,C.getUsers().at(i)->getTeacher()))
-				 cout << "Error adding Lesson. Try again"<< endl;*/
+				if (save == 1) {
+					//save function
+				}
+				return 0;
+				break;
 
 			}
-
-			break;
-
-		}
-		case 3: {
-			++D;
-			break;
-		}
-		case 4: {
-			cout << "Save and exit? " << endl;
-			cout << "1.Yes 0.No" << endl;
-			cin >> save;
-
-			if (save == 1) {
-				//save function
-			}
-			return 0;
-
-		}
 		}
 
 	}
@@ -212,58 +226,44 @@ void DevelopCompany(Company &C)
 
 
 
-int main()
-{
+int main() {
+
 	int cardValue = 1;
 
+
+	unsigned int year, day, month;
 	////////////////////////////////////
-	///////////////////
-	string name,gender;
-	int age;
-	bool isGold;
-	//////////////////
-	int m,d,strH, dur;
-	//double price;
-	//duration -- unsigned int --problem?
-	int duration;
-    /////////////////
-    int save;
-    unsigned int year, day,month;
-	////////////////////////////////////
+
 	int flag = MainIntro();
 
-	switch (flag)
-	{
-	case 1:
-	{
-		cout << "Please enter date (day month year): "<< endl;
-		cin>> day;
-		cin>> month;
-		cin>> year;
+	switch (flag) {
+		case 1: {
+			cout << "Please enter date (day month year): " << endl;
+			cin >> day;
+			cin >> month;
+			cin >> year;
 
-		Date D(day,month,year);
+			Date D(day, month, year);
 
-		int flagMenu = 0;
+			Company C(cardValue, year);
+			cardValue++;
+			DevelopCompany(C,D, cardValue);
 
-		Company C(cardValue, year);
-		cardValue++;
+			break;
 
-		break;
-
-	}
-	case 2:
-	{
-		//load
-		break;
-	}
-	case 3:
-	{
-		return 0;
-		break;
-	}
+		}
+		case 2: {
+			//load
+			//DevelopCompany(C, cardValue);
+			break;
+		}
+		case 3: {
+			return 0;
+			break;
+		}
 	}
 
-	DevelopCompany(C);
+
 
 }
 
