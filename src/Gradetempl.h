4a200fa4f02dbd4de210d84ae3a5dd856971650d
João Miguel Martins <up201707311@fe.up.pt>
2018-11-18 01:59:37 +0000

    Altered grading template to eliminate additional comments

--- src/Gradetempl.h
+++ src/Gradetempl.h
@@ -11,44 +11,39 @@
 #include <iostream>
 #include<string>
 
-void grade_1(std::ostream &os, std::string addcomm)
+void grade_1(std::ostream &os)
 {
 	os << "\t The student did very poorly in this month. Effort and progress far from meeting the goals set." << std::endl
 	<< "\t Next month requires change of training habits and behavior from the student." << std::endl
-	<< std::endl << "\t The grade set to this month was (scale of 1-5): 1 " << std::endl << std::endl
-	<< "\t Additional comments: " << std::endl << "\t " << addcomm << std::endl;
+	<< std::endl << "\t The grade set to this month was (scale of 1-5): 1 " << std::endl << std::endl;
 }
 
-void grade_2(std::ostream &os, std::string addcomm)
+void grade_2(std::ostream &os)
 {
 	os << "\t The student's this month's performance is unsatisfactory . Effort and progress weren't enough to meet the goals set." << std::endl
 	<< "\t Next month requires improvement on the effort and practice hours of the student." << std::endl
-	<< std::endl << "\t The grade set to this month was (scale of 1-5): 2 " << std::endl << std::endl
-	<< "\t Additional comments: " << std::endl << "\t " << addcomm << std::endl;
+	<< std::endl << "\t The grade set to this month was (scale of 1-5): 2 " << std::endl << std::endl;
 }
 
-void grade_3(std::ostream &os, std::string addcomm)
+void grade_3(std::ostream &os)
 {
 	os << "The student performance on this month is sufficient. Effort and progress met the goals set." << std::endl
 	<< "If the student improves effort and raises practice time better results will be achieved." << std::endl
-	<< std::endl << "\t The grade set to this month was (scale of 1-5): 3 " << std::endl << std::endl
-	<< "\t Additional comments: " << std::endl << "\t " << addcomm << std::endl;
+	<< std::endl << "\t The grade set to this month was (scale of 1-5): 3 " << std::endl << std::endl;
 }
 
-void grade_4(std::ostream &os, std::string addcomm)
+void grade_4(std::ostream &os)
 {
 	os << "The student evaluation this month is good. The goals set were met very adequately." << std::endl
 	<< "If the student pushes himself/herself further, excellence is waiting just around the corner." << std::endl
-	<< std::endl << "\t The grade set to this month was (scale of 1-5): 4" << std::endl << std::endl
-	<< "\t Additional comments: " << std::endl << "\t " << addcomm << std::endl;
+	<< std::endl << "\t The grade set to this month was (scale of 1-5): 4" << std::endl << std::endl;
 }
 
-void grade_5(std::ostream &os, std::string addcomm)
+void grade_5(std::ostream &os)
 {
 	os << "The student evaluation for this month is excellent! Effort and progress went beyond the goals set." << std::endl
 	<< "Congratulations for this month's performance, same results are to expect on next month's review." << std::endl
-	<< std::endl << "\t The grade set to this month was (scale of 1-5): 5 " << std::endl << std::endl
-	<< "\t Additional comments: " << std::endl << "\t " << addcomm << std::endl;
+	<< std::endl << "\t The grade set to this month was (scale of 1-5): 5 " << std::endl << std::endl;
 }
 
 
